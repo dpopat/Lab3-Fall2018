1aa18985f259d0fb8347fe7f3a21607dd7d3c163
DhvanilPopat <dpopat99@gmail.com>
2018-09-19 12:20:14 -0500

    Completed Lab 3.

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,47 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
     public static void main(String[] unused) {
-        // write your code here
+       String story = urlToString("http://erdani.com/tdpl/hamlet.txt");
+       System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
+       System.out.println(story);
+       System.out.println(uniqueWordCounter(story, "Hamlet"));
+    }
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
+    public static int wordCount(String input) {
+        String[] lines = input.split("\\s+");
+        int sum = lines.length;
+        return sum;
+    }
+
+    public static int uniqueWordCounter(String input, String word) {
+        String[] lines = input.split("\\s+");
+        int counter = 0;
+        for (int i = 0; i < lines.length; i++){
+            if(lines[i].equals(word)){
+                counter++;
+            }
+        }
+        return counter;
     }
 }
